465cfcdf6b030ece2deebd269b985df70de4cfb0
Brad Spurgin <brad.spurgin@charter.com>
2024-09-01 13:42:17 -0600

    adding hilt

---
+++ app/src/main/java/com/spurgin/starwars/Application.kt
@@ -0,0 +1,9 @@
+package com.spurgin.starwars
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+    // You can override methods like onCreate() if needed
+}
